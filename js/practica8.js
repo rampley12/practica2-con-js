5cc81e46ac3ec31ff8165a824f14deb718f92972
Ramiro Diaz <ramirodiaz6780@gmail.com>
2023-11-05 17:34:07 -0300

    modifique algunas cosas que me eqiboque en el trabjo numero 8

--- js/practica8.js
+++ js/practica8.js
@@ -1,12 +1,12 @@
 function generarPiramide() {
-    var numero = parseInt(prompt("Ingrese un número (no mayor a 50):"));
+    const numero = parseInt(prompt("Ingrese un número (no mayor a 50):"));
     if (numero > 50) {
         alert("El número ingresado es mayor a 50. Por favor, ingrese un número válido.");
         return;
     }
-    var piramide = "";
-    for (var i = 1; i <= numero; i++) {
-        for (var j = 1; j <= i; j++) {
+    const piramide = "";
+    for (let i = 1; i <= numero; i++) {
+        for (let j = 1; j <= i; j++) {
             piramide += j;
         }
         piramide += "<br>";
