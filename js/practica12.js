b303855cbcaeee6c37096919ad12a50a1f852034
Ramiro Diaz <ramirodiaz6780@gmail.com>
2023-11-06 13:42:50 -0300

    agregue el trabajo 12 donde te d un numero aleatorio

---
+++ js/practica12.js
@@ -0,0 +1,5 @@
+// Generar un número aleatorio entre 1 y 99
+const numeroAleatorio = Math.floor(Math.random() * 99) + 1;
+
+// Mostrar el número aleatorio generado
+document.write("El número aleatorio es: " + numeroAleatorio);
